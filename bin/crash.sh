328a72e3d8810262007528a514cb21c249c0fa47
Julien Viet <julien@julienviet.com>
2012-04-09 18:22:56 +0200

    update to latest snapshot

--- bin/crash.sh
+++ bin/crash.sh
@@ -27,4 +27,4 @@ done
 # Create tmp dir if it does not exist
 mkdir -p $CRASH_HOME/tmp
 
-java -Djava.io.tmpdir=$CRASH_HOME/tmp -Djava.util.logging.config.file=$CRASH_HOME/conf/logging.properties -jar $CRASH_HOME/bin/crsh.shell.core-1.0.0-cr2-SNAPSHOT-standalone.jar $EXT_JARS --cmd $CRASH_HOME/cmd --property crash.vfs.refresh_period=1 $@
+java -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Djava.io.tmpdir=$CRASH_HOME/tmp -Djava.util.logging.config.file=$CRASH_HOME/conf/logging.properties -jar $CRASH_HOME/bin/crsh.shell.core-1.0.0-cr2-SNAPSHOT-standalone.jar $EXT_JARS --cmd $CRASH_HOME/cmd --property crash.vfs.refresh_period=1 $@
